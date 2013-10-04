ea769af045a15975cb70a4258e4a8d3f09e52408
wagtra12 <twags32@gmail.com>
2013-10-04 15:15:23 -0700

    hw10

---
+++ hw1.py.py
@@ -0,0 +1,55 @@
+# Name: Travis Wagner
+# Evergreen Login: wagtra12
+# Computer Science Foundations
+# Programming as a Way of Life
+# Homework 1
+
+# You may do your work by editing this file, or by typing code at the
+# command line and copying it into the appropriate part of this file when
+# you are done.  When you are done, running this file should compute and
+# print the answers to all the problems.
+
+import math                     # makes the math.sqrt function available
+
+
+###
+### Problem 1
+###
+print "Problem 1 solution follows:"
+
+print str (5.86+math.sqrt((5.86**2)-4*8.5408)/2)
+print str (5.86-math.sqrt((5.86**2)-4*8.5408)/2)
+
+# ... write your code and comments here (and remove this line)
+
+
+###
+### Problem 2
+###
+print "Problem 2 solution follows:"
+
+from hw1_test import *
+print a
+print b
+print c
+print d
+print e
+print f
+
+# ... write your code and comments here (and remove this line)
+
+
+###
+### Problem 3
+###
+
+print "Problem 3 solution follows:"
+
+print ((a and b) or (not c) and not (d or e or f))
+
+
+
+
+
+
+# Jordan Rawls
